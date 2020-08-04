ea5a66d313baf4b175a29507eb30d90b4fff7aac
wyuefzhuol <1527169088@qq.com>
2020-08-04 14:36:51 +0800

    [Todo-list][Eric] feature: initialize the todo-list redux project

---
+++ src/action/actionTypes.js
@@ -0,0 +1,3 @@
+export const ADD_STRING = 'ADD_STRING';
+
+export const DELETE_STRING = 'DELETE_STRING';
---
+++ src/reducer/stringReducer.js
@@ -0,0 +1,10 @@
+import { ADD_STRING } from '../action/actionTypes'
+import { createReducer } from '@reduxjs/toolkit'
+
+const initializeState = {
+    stringList: []
+}
+
+export default createReducer (initializeState, {
+    [ADD_STRING]: ((state, action) => ({ stringList: state.stringList.push(action.payload.string) }))
+})
