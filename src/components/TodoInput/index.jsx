7121f61f56774230777cec4c71935e8d511de66e
wyuefzhuol <1527169088@qq.com>
2020-08-04 14:48:59 +0800

    [Todo-list][Oliver] feature: create todo input and button

---
+++ src/components/TodoInput/index.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class TodoInput extends Component {
+    render() {
+        return (<div>
+            <input />
+            <button>add</button>
+        </div>)
+    }
+}
+
+export default TodoInput;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
+import store from './store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
