73f3f1311d4c14ec979d17bd546a49dbfadff101
wyuefzhuol <1527169088@qq.com>
2020-08-04 14:52:32 +0800

    [Todo-list][Eric] feature: create todoitem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import TodoInput from './components/TodoInput'
+import TodoItem from './components/TodoItem';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <TodoInput />
+        <TodoItem />
       </header>
     </div>
   );
---
+++ src/components/TodoItem/index.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class TodoItem extends Component {
+    render() {
+        return (<div>
+            <label></label>
+            <button>×</button>
+        </div>)
+    }
+}
+
+export default TodoItem;
