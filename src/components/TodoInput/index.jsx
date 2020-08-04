359b33e0b850498b700409fe7a9f8153f40ac8cd
wyuefzhuol <1527169088@qq.com>
2020-08-04 20:40:57 +0800

    [Todo-list][Oliver] feature: A POST request to add data is implemented

--- src/components/TodoInput/index.jsx
+++ src/components/TodoInput/index.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
 import { addItemAction } from '../../action/addItemAction'
 import { connect } from 'react-redux'
+import Axios from 'axios'
 
 class TodoInput extends Component {
     addItem = () => {
-        let inputString = {
+        const inputString = {
             content: document.getElementById('inputString').value,
             status: false
         }
+        const _this = this
         if (document.getElementById('inputString').value !== '') {
-            this.props.addItem(inputString);
+            Axios.post('https://5e9ec500fb467500166c4658.mockapi.io/todos', inputString)
+                .then(function (response) {
+                    _this.props.addItem(response.data)
+                    alert('添加成功！')
+                })
+                .catch(function (error) {
+                    alert(error)
+                })
             document.getElementById('inputString').value = '';
         } else {
             alert('请输入后再点击添加！');
