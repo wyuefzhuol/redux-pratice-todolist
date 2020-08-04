86705070d85e09c39bedc5e4c31999af4a7395af
wyuefzhuol <1527169088@qq.com>
2020-08-04 20:48:10 +0800

    [Todo-list][Eric] feature:  A DELETE request to delete data is implemented

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,10 +1,16 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux';
 import { deleteItemAction, makeTodoAction } from '../../action/addItemAction'
+import Axios from 'axios';
 
 class TodoItem extends Component {
     deleteItem = () => {
-        this.props.deleteItem(this.props.stringIndex);
+        const _this = this
+        const deleteUrl = 'https://5e9ec500fb467500166c4658.mockapi.io/todos/'+this.props.stringItem.id
+        Axios.delete(deleteUrl).then(function (response) {
+            _this.props.deleteItem(_this.props.stringIndex)
+            alert(response.data.content+'删除成功！')
+        })
     }
 
     makeTodo = () => {
