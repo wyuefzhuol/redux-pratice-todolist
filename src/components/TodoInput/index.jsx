ecd0272b20987dc0d9e962974fadeabfabbaf9cb
wyuefzhuol <1527169088@qq.com>
2020-08-07 08:36:55 +0800

    make todo: input value should be empty after add todo item

--- src/components/TodoInput/index.jsx
+++ src/components/TodoInput/index.jsx
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
 import { Input, Divider } from 'antd';
 
-//TODO: input框输入后要清空
 class TodoInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+        };
+    }
+
     addItem = (value) => {
         const inputString = {
             content: value,
             status: false
         }
         this.props.addItem(inputString);
-        this.setState({ inputString: '' });
+        this.setState({ value: '' });
+    }
+
+    updateInputValue = (event) => {
+        this.setState({ value: event.target.value });
     }
 
     render() {
         return (<div>
             <Divider orientation="left">Add Todo</Divider>
             <Input.Search
+                value={this.state.value}
                 placeholder="what do you want to do"
                 enterButton="add"
                 size="large"
                 onSearch={value => this.addItem(value)}
+                onChange={this.updateInputValue}
             />
         </div>)
     }
